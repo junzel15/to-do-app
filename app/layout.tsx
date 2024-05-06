6d3f1dbeb5c2b2793a37ea88a55b601432127233
Elvis H. Bogsangit Jr. <bogsangitelvis@gmail.com>
2024-05-06 12:34:30 +0800

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import Navigation from "../components/Navigation";
+
 import "./globals.css";
-console.log(
-  "process.env.NEXT_PUBLIC_SUPABASE_URL",
-  process.env.NEXT_PUBLIC_SUPABASE_URL
-);
+
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <>
